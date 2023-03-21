dc13edabb4e13371d74c3abcf6da396759761aa2
yeonjae <yeoonjae5310@gmail.com>
2023-03-22 00:28:21 +0900

    init commit

---
+++ src/main/java/com/bside/threepick/ThreePickApplication.java
@@ -0,0 +1,13 @@
+package com.bside.threepick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreePickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreePickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bside/threepick/ThreePickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bside.threepick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreePickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
