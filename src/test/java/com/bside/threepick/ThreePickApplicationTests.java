8f4f3536a174168d774af2b272bc8a029b210b18
yeonjae <yeonjae5310@gmail.com>
2023-04-30 23:03:57 +0900

    fix: build fail

--- src/test/java/com/bside/threepick/ThreePickApplicationTests.java
+++ src/test/java/com/bside/threepick/ThreePickApplicationTests.java
@@ -1,13 +1,7 @@
 package com.bside.threepick;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ThreePickApplicationTests {
-
-  @Test
-  void contextLoads() {
-  }
-
 }
